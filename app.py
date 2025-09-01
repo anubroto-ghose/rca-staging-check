c1d72ee9275c19cfbb1a09c46fd6ca440f5ed91d
Anubroto <anubroto.g@prodapt.com>
2025-09-01 19:30:55 +0530

    Add global exception handler to app.py

--- app.py
+++ app.py
@@ -7,3 +7,10 @@ def main():
   return {
     "content": "root"
 }
+
+@app.exception_handler(Exception)
+async def exception_handler(request, exc):
+    return JSONResponse(
+        status_code=500,
+        content={"message": "An error occurred: " + str(exc)},
+    )
